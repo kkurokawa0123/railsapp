1123b3c5198b2a8d9f5a327d6e5ec840dd87097b
kkurokawa <k.kurokawa.shineforce@gmail.com>
2026-08-30 18:07:02 +0900

    feat: Tailwind CSSの導入に伴うフロントエンド構成の更新とAppコンポーネントのリファクタリング、使用していない App.css および関連するアセットを削除。

---
+++ frontend/src/presentation/App.tsx
@@ -0,0 +1,10 @@
+const App: React.FC = () => {
+  return (
+  <div className="text-3xl font-bold">
+    Hello Tailwind CSS!
+  </div>
+
+);
+};
+
+export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +9,5 @@ export default defineConfig({
     host: true,
     port: 8000,
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
